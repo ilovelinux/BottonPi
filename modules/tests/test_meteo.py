f3e86e6c14183a4905551d7a04ddd25614b4ce32
Antonio Spadaro <antoniospadaro45@gmail.com>
2015-06-11 10:53:47 +0000

    Added tests for call weather with It language

--- modules/tests/test_meteo.py
+++ modules/tests/test_meteo.py
@@ -1,20 +1,30 @@
 import unittest
-import sys
-sys.path += ['/home/pi/.irc3/modules/']
-import meteo
+from modules import meteo
 
 class OpenWeatherMapTest(unittest.TestCase):
     def test_openweathermap(self):
-        self.assertIn('Rome', meteo.cmd_opm({'<city>':['Rome']}))
+        self.assertIn('Rome',
+                      meteo.cmd_opm({'<city>':['Rome']}))
     def test_openweathermap_space(self):
-        self.assertIn('New York', meteo.cmd_opm({'<city>':['New', 'York']}))
+        self.assertIn('New York',
+                      meteo.cmd_opm({'<city>':['New', 'York']}))
     def test_openweathermap_no_result(self):
-        self.assertEqual('Nessun risultato per "*"', meteo.cmd_opm({'<city>':['*']}))
+        self.assertEqual('No results for "*"',
+                         meteo.cmd_opm({'<city>':['*']}))
+    def test_openweathermap_no_result_it(self):
+        self.assertEqual('Nessun risultato per "*"',
+                         meteo.cmd_opm({'<city>':['*']}, 'it'))
 
 class YahooWeatherTest(unittest.TestCase):
     def test_yahooweather(self):
-        self.assertIn('Rome', meteo.cmd_ym({'<city>':['Rome']}))
+        self.assertIn('Rome', 
+                      meteo.cmd_ym({'<city>':['Rome']}))
     def test_yahooweather_space(self):
-        self.assertIn('New York', meteo.cmd_ym({'<city>':['New', 'York']}))
+        self.assertIn('New York',
+                      meteo.cmd_ym({'<city>':['New', 'York']}))
     def test_yahooweather_no_result(self):
-        self.assertIn('Nessun risultato per "*"', meteo.cmd_ym({'<city>':['*']}))
+        self.assertEqual('No results for "*"',
+                         meteo.cmd_ym({'<city>':['*']}))
+    def test_yahooweather_no_result_it(self):
+        self.assertEqual('Nessun risultato per "*"',
+                         meteo.cmd_ym({'<city>':['*']}, 'it'))
